2483fae815f388f18158c21e132d3aa69c96a81a
KhasMek <Boushh@gmail.com>
2011-03-15 18:10:16 -0700

    modified to more easily work with the fasinate

---
+++ products/mau5_fascinate.mk
@@ -0,0 +1,20 @@
+# Inherit AOSP device configuration for fascinate.
+$(call inherit-product, device/samsung/fascinate/full_fascinate.mk)
+
+PRODUCT_NAME := mau5_fascinate
+
+# Set Default Ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=SometimesThingsGet.ogg
+
+PRODUCT_COPY_FILES += \
+    vendor/ctso/prebuilt/common/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
+    vendor/ctso/prebuilt/common/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf \
+    vendor/ctso/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh \
+    vendor/ctso/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg
+
+PRODUCT_PACKAGES += Mau5Wallpapers
+PRODUCT_PACKAGES += ThemeDefaults
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/mau5/overlay
+
