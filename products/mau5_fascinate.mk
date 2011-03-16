93433a155cd79926436a7630c9d5737ef9ad3ec1
KhasMek <Boushh@gmail.com>
2011-03-15 19:18:50 -0700

    fascinate compatability

--- products/mau5_fascinate.mk
+++ products/mau5_fascinate.mk
@@ -10,11 +10,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/ctso/prebuilt/common/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
     vendor/ctso/prebuilt/common/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf \
-    vendor/ctso/prebuilt/common/bin/setdefcols.sh:system/bin/setdefcols.sh \
     vendor/ctso/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/mau5/overlay
 
 PRODUCT_PACKAGES += Mau5Wallpapers
 PRODUCT_PACKAGES += ThemeDefaults
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/mau5/overlay
 
